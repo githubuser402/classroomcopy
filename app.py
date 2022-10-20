da22a42bf96f1202a01eb0ca247aaca321c2f0c2
Arty <colomborin53@gmail.com>
2022-10-20 12:44:28 +0300

    save before code review

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
-from config.database import DevelopmentConfigMySQL
-from config.constants import Constants
-from utils.database import db
-from utils.schema import ma
-from utils.migrate import migrate
 
+from config.constants import Constants
+from config.database import DevelopmentConfigMySQL
 from models.class_model import Class
 from models.task_model import Task
 from models.user_model import User
+from utils.database import db
+from utils.migrate import migrate
+from utils.schema import ma
 
 
 def create_app():
